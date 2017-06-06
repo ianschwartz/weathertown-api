4663b819821834bd5e706f45481f0773a81c9147
Schwartz <ian@schwartz.world>
2017-06-06 16:03:18 +0000

    Added all restful actions to the ReadingsController

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,2 +1,47 @@
 class ReadingsController < ApplicationController
+
+  before_action :set_station
+  before_action :set_station_reading, only: [:show, :update, :destroy]
+
+  # GET /stations/:station_id/readings
+  def index
+    json_response(@station.readings)
+  end
+
+  # GET /stations/:station_id/readings/:id
+  def show
+    json_response(@reading)
+  end
+
+  # post /stations/:station_id/readings
+  def create
+    @station.readings.create!(reading_params)
+    json_response(@station, :created)
+  end
+
+  # PUT /stations/:station_id/readings/:id
+  def update
+    @reading.update(reading_params)
+    head :no_content
+  end
+
+  # DELETE /stations/:station_id/readings/:id
+  def destroy
+    @reading.destroy
+    head :no_content
+  end
+
+  private
+
+  def reading_params
+    params.permit(:temp, :humidity)
+  end
+
+  def set_station
+    @station = Station.find(params[:station_id])
+  end
+
+  def set_station_reading
+    @reading = @station.readings.find_by!(id: params[:id]) if @station
+  end
 end
